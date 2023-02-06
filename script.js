c24098912b0d870b3b446954957ec130270182b6
AlliedRx <124627529+AlliedRx@users.noreply.github.com>
2023-02-06 15:19:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const form = document.querySelector("form");
+const taskInput = document.querySelector("#task");
+const tasksList = document.querySelector("#tasks");
+
+form.addEventListener("submit", addTask);
+tasksList.addEventListener("click", completeTask);
+
+function addTask(e) {
+  e.preventDefault();
+
+  if (!taskInput.value) return;
+
+  const taskHTML = `
+    <li class="task">
+      <input type="checkbox">
+      <span class="task-text">${taskInput.value}</span>
+      <button class="delete-task">Delete</button>
+    </li>
+  `;
+
+  tasksList.insertAdjacentHTML("beforeend", taskHTML);
+  taskInput.value = "";
+}
+
+function completeTask(e) {
+  if (!e.target.matches("input")) return;
+
+  const task = e.target.parentElement;
+  task.querySelector(".task-text").classList.toggle("completed");
+}
 const form = document.getElementById("form");
 const taskInput = document.getElementById("task");
 const list = document.getElementById("list");
